9b2cfe02f59eb0ed4bbca0955820412a51c3b2f7
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-03-11 16:30:49 +0200

    Creates Employee class that extends IdentityUser

---
+++ LeaveManagement.Web/Data/Employee.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LeaveManagement.Web.Data
+{
+    public class Employee : IdentityUser
+    ///<summary>
+    /// Class to extend IdentityUser and add the
+    /// following table columns.
+    /// </summary>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string TaxId { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}

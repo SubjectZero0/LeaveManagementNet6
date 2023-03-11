dc3855ac12cc6e01d13cd71dbf5a02be5ed48c56
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-03-11 16:31:06 +0200

    Creates leave classes

---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    ///<summary>
+    ///Table for keeping track of allocated leaves
+    /// </summary>
+    {
+        ///<remarks>
+        ///LeaveType is a FK
+        /// </remarks>
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+        /// <remarks>
+        /// EmployeeId is NOT  FK yet
+        /// </remarks>
+        public string EmployeeId { get; set; }
+    }
+}
---
+++ LeaveManagement.Web/Data/LeaveType.cs
@@ -0,0 +1,18 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public class LeaveType : BaseEntity
+    ///<summary>
+    ///
+    /// Type of Leave Request
+    /// Used by Administrators
+    ///
+    /// </summary>
+    {
+        /// <remark>
+        /// Name is added by admins
+        /// </remark>
+        public string Name { get; set; }
+
+        public int DefaultDays { get; set; }
+    }
+}

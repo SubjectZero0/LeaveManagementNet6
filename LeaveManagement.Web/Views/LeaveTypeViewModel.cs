d122e704485ec1eddb9e1ff163c359413f6bb369
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-03-12 18:50:55 +0200

    makes fields required

--- LeaveManagement.Web/Views/LeaveTypeViewModel.cs
+++ LeaveManagement.Web/Views/LeaveTypeViewModel.cs
@@ -7,9 +7,11 @@ namespace LeaveManagement.Web.Views
         public int Id { get; set; }
 
         [Display(Name ="Leave Type Name")]
+        [Required]
         public string Name { get; set; }
 
         [Display(Name = "Default Number of Days")]
+        [Required]
         public int DefaultDays { get; set; }
     }
 }

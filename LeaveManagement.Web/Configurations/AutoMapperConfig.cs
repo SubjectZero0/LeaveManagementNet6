b6b6c441f9516b53ec0cb94380ade93aea2ab848
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-03-12 14:01:12 +0200

    Creates AutoMapper Configurations for Leave Types

---
+++ LeaveManagement.Web/Configurations/AutoMapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LeaveManagement.Web.Data;
+using LeaveManagement.Web.Views;
+
+namespace LeaveManagement.Web.Configurations
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeViewModel>().ReverseMap();
+        }
+    }
+}

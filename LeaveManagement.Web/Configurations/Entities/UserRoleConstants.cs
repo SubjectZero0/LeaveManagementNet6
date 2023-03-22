869bfe6399cb424bf52c6f009cec0d3dee3a9558
SubjectZero0 <iordanoualexander@gmail.com>
2023-03-22 14:31:29 +0200

    Creates Constant for roles to avoid spelling errors

---
+++ LeaveManagement.Web/Configurations/Entities/UserRoleConstants.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace LeaveManagement.Web.Configurations.Entities
+{
+    public static class UserRoleConstants
+    {
+        public const string AdminRoleId = "4cge9abc - 6741 - 473f - 907e - 15e96893788e";
+        public const string UserRoleId = "4cge7abc - 7641 - 473f - 907e - 15e88893788e";
+        public const string Administrator = "Administrator";
+        public const string User = "User";
+
+
+    }
+}

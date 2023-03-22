c410ee6ead86053167ce5bc8a4467d47c161881e
SubjectZero0 <iordanoualexander@gmail.com>
2023-03-22 13:51:06 +0200

    Applies Role and Seeded Role Configurations

--- LeaveManagement.Web/Data/ApplicationDbContext.cs
+++ LeaveManagement.Web/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LeaveManagement.Web.Configurations.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagement.Web.Data
@@ -10,6 +11,13 @@ namespace LeaveManagement.Web.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new RoleSeedConfig()); //initializes DbContext with Roles
+            builder.ApplyConfiguration(new UserSeedConfig()); //initializes DbContext with a user
+            builder.ApplyConfiguration(new UserRoleConfig()); //the initiated user starts as an Admininstrator
+        }
         public DbSet<LeaveType> LeaveTypes { get; set; }
         public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
     }

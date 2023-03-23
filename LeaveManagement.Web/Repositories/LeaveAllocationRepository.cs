d1ac797d446e0ca2e855be7eb906a27992bb490f
SubjectZero0 <iordanoualexander@gmail.com>
2023-03-23 16:10:46 +0200

    Creates Interface and implements it for Leave Allocations

---
+++ LeaveManagement.Web/Repositories/LeaveAllocationRepository.cs
@@ -0,0 +1,12 @@
+﻿using LeaveManagement.Web.Contracts;
+using LeaveManagement.Web.Data;
+
+namespace LeaveManagement.Web.Repositories
+{
+    public class LeaveAllocationRepository : GenericRepository<LeaveAllocation>, ILeaveAllocationsRepository
+    {
+        public LeaveAllocationRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}

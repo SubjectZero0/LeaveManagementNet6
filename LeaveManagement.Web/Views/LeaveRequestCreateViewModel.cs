92b8fc024faf9a6564ebc540f96341fb2b4ae9b7
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-04-02 22:19:58 +0300

    Adds Leave Request Views

---
+++ LeaveManagement.Web/Views/LeaveRequestCreateViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Views
+{
+    public class LeaveRequestCreateViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Type")]
+        [Required]
+        public int LeaveTypeId { get; set; }
+
+        [Display(Name = "Start Date")]
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateStarted { get; set; }
+
+        [Display(Name = "End Date")]
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateEnded { get; set; }
+
+        [Display(Name = "Description")]
+        public string? Comment { get; set; }
+    }
+}

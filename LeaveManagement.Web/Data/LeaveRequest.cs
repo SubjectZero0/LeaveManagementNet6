c5e6feb8c3c88cf5e0f2972dfeaa7f827dcd5664
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-04-02 22:21:47 +0300

    Adds Leave Request Model and table in ApplicationDbContext

---
+++ LeaveManagement.Web/Data/LeaveRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveRequest : BaseEntity
+    {
+        public DateTime DateStarted { get; set; }
+
+        public DateTime DateEnded { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+        public bool? IsApproved { get; set; }
+
+        public bool IsCancelled { get; set; }
+
+        public string? Comment { get; set; }
+
+        public string RequestingEmployeeId { get; set; }
+    }
+}

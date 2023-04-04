137b6abce61a7ca08acf05acdfdd6118174dec80
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-04-04 13:51:00 +0300

    Creates and implements GetMyLeavesAsync & GetAllEmployeeRequestsAsync methods

--- LeaveManagement.Web/Contracts/ILeaveRequestRepository.cs
+++ LeaveManagement.Web/Contracts/ILeaveRequestRepository.cs
@@ -1,12 +1,17 @@
 ﻿using LeaveManagement.Web.Data;
+using LeaveManagement.Web.Views;
 using System.Security.Claims;
 
 namespace LeaveManagement.Web.Contracts
 {
     public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
     {
-        Task CreateWithCurrentUser(LeaveRequest leaveRequest, ClaimsPrincipal user);
+        Task CreateWithCurrentUser(LeaveRequest leaveRequest);
 
         Task<ClaimsPrincipal> GetCurrentUser();
+
+        Task<EmployeeLeavesListViewModel> GetMyLeavesAsync();
+
+        Task<List<LeaveRequest>> GetAllEmployeeRequestsAsync(string employeeId);
     }
 }
--- LeaveManagement.Web/Repositories/LeaveRequestRepository.cs
+++ LeaveManagement.Web/Repositories/LeaveRequestRepository.cs
@@ -1,5 +1,9 @@
-﻿using LeaveManagement.Web.Contracts;
+﻿using AutoMapper;
+using LeaveManagement.Web.Contracts;
 using LeaveManagement.Web.Data;
+using LeaveManagement.Web.Views;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Security.Claims;
 
 namespace LeaveManagement.Web.Repositories
@@ -8,15 +12,27 @@ namespace LeaveManagement.Web.Repositories
     {
         private readonly ApplicationDbContext _context;
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ILeaveAllocationsRepository _leaveAllocationsRepository;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
 
-        public LeaveRequestRepository(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor) : base(context)
+        public LeaveRequestRepository(ApplicationDbContext context,
+                                      IHttpContextAccessor httpContextAccessor,
+                                      ILeaveAllocationsRepository leaveAllocationsRepository,
+                                      ILeaveTypeRepository leaveTypeRepository,
+                                      IMapper mapper) : base(context)
         {
             _context = context;
             _httpContextAccessor = httpContextAccessor;
+            _leaveAllocationsRepository = leaveAllocationsRepository;
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
         }
 
-        public async Task CreateWithCurrentUser(LeaveRequest leaveRequest, ClaimsPrincipal user)
+        public async Task CreateWithCurrentUser(LeaveRequest leaveRequest)
         {
+            var user = await GetCurrentUser();
+
             if (user is not null)
             {
                 leaveRequest.IsCancelled = false;
@@ -33,6 +49,13 @@ namespace LeaveManagement.Web.Repositories
             }
         }
 
+        public async Task<List<LeaveRequest>> GetAllEmployeeRequestsAsync(string employeeId)
+        {
+            var requests = await _context.LeaveRequests.Where(x => x.RequestingEmployeeId == employeeId).ToListAsync();
+
+            return requests;
+        }
+
         public async Task<ClaimsPrincipal> GetCurrentUser()
         {
             var user = _httpContextAccessor.HttpContext?.User;
@@ -41,6 +64,28 @@ namespace LeaveManagement.Web.Repositories
                 return await Task.FromResult(user);
             }
             throw new Exception("You are not an authenticated user");
+        }
+
+        public async Task<EmployeeLeavesListViewModel> GetMyLeavesAsync()
+        {
+            var user = await GetCurrentUser();
+
+            // get the model lists
+            var leaveAllocations = await _leaveAllocationsRepository.GetAllByEmployeeAsync(user.FindFirstValue(ClaimTypes.NameIdentifier));
+            var leaveRequests = await GetAllEmployeeRequestsAsync(user.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            // get the ViewModel lists
+            var leaveAllocationsVM = _mapper.Map<List<LeaveAllocationsListViewModel>>(leaveAllocations);
+            var leaveRequestsVM = _mapper.Map<List<LeaveRequestsListViewModel>>(leaveRequests);
+
+            // create a new instance of EmployeeLeavesListViewModel
+            var employeeLeavesList = new EmployeeLeavesListViewModel()
+            {
+                LeaveAllocations = leaveAllocationsVM,
+                LeaveRequests = leaveRequestsVM
+            };
+
+            return employeeLeavesList;
         }
     }
 }

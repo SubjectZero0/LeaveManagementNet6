b42e46e9c492958945236e6ff1d73bc135e04462
Alexandrer Iordanou <iordanoualexander@gmail.com>
2023-04-08 18:55:23 +0300

    New Views and View models for Admin

---
+++ LeaveManagement.Web/Views/AdminLeaveRequestsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using LeaveManagement.Web.Data;
+
+namespace LeaveManagement.Web.Views
+{
+    public class AdminLeaveRequestsListViewModel : LeaveRequestsListViewModel
+    {
+        public string RequestingEmployeeId { get; set; }
+
+        public EmployeesViewModel RequestingEmployee { get; set; }
+    }
+}
---
+++ LeaveManagement.Web/Views/AdminLeaveStatisticsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Views
+{
+    public class AdminLeaveStatisticsViewModel
+    {
+        public int Approved { get; set; }
+
+        public int Rejected { get; set; }
+
+        public int Pending { get; set; }
+
+        public int Cancelled { get; set; }
+
+        public int Total { get; set; }
+
+        [Display(Name = "Total Days Approved")]
+        public int LeaveDaysApproved { get; set; }
+    }
+}
